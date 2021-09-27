beffe68294f54ca2d1182e20e971702d240dda77
Lee Jaeheon <ti6419@gmail.com>
2021-09-28 01:19:56 +0900

    first commit

---
+++ src/main/java/com/violetbeach/studyrestapi/StudyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.studyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/violetbeach/studyrestapi/StudyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.studyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
