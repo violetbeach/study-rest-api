c80bc3006ba7b735f6e42f1f70dca3ffd92f16a8
Lee Jaeheon <ti6419@gmail.com>
2021-10-14 12:34:55 +0900

    feat: Add event DTO

--- src/main/java/com/violetbeach/studyrestapi/StudyRestApiApplication.java
+++ src/main/java/com/violetbeach/studyrestapi/StudyRestApiApplication.java
@@ -1,13 +1,20 @@
 package com.violetbeach.studyrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudyRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyRestApiApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/violetbeach/studyrestapi/events/EventDto.java
+++ src/main/java/com/violetbeach/studyrestapi/events/EventDto.java
@@ -1,17 +1,14 @@
 package com.violetbeach.studyrestapi.events;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class EventDto {
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
-@Entity
-public class Event {
-
-    @Id @GeneratedValue
-    private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -22,9 +19,5 @@ public class Event {
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
 
 }
