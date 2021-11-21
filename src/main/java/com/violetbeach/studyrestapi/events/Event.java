a357acd5aeb08b1483ddfffac7e388e3c42e77cc
Lee Jaeheon <ti6419@gmail.com>
2021-11-21 18:24:22 +0900

    feat: Add business logic test of event

--- src/main/java/com/violetbeach/studyrestapi/events/Event.java
+++ src/main/java/com/violetbeach/studyrestapi/events/Event.java
@@ -27,4 +27,19 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 
+    public void update() {
+        // Update free
+        if (this.basePrice == 0 && this.maxPrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+        // Update offLine
+        if (this.location == null || this.location.isBlank()) {
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+    }
+
 }
