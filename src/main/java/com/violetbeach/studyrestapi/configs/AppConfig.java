a000701d6b442cf92c348cb119be09fd92071bb8
Lee Jaeheon <ti6419@gmail.com>
2021-12-04 13:52:56 +0900

    feat: Add spring security form authenticate

--- src/main/java/com/violetbeach/studyrestapi/configs/AppConfig.java
+++ src/main/java/com/violetbeach/studyrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package com.violetbeach.studyrestapi.configs;
 
+import com.violetbeach.studyrestapi.accounts.Account;
+import com.violetbeach.studyrestapi.accounts.AccountRole;
+import com.violetbeach.studyrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,6 +25,25 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account account = Account.builder()
+                        .email("ti6419@gmail.com")
+                        .password("violetbeach")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(account);
+            }
+        };
     }
 
 }
