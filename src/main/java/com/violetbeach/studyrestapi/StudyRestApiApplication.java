e502acad24592540bfe5ea711c4525894429a06a
Lee Jaeheon <ti6419@gmail.com>
2021-12-04 13:38:21 +0900

    feat: Add spring security config

--- src/main/java/com/violetbeach/studyrestapi/StudyRestApiApplication.java
+++ src/main/java/com/violetbeach/studyrestapi/StudyRestApiApplication.java
@@ -1,20 +1,13 @@
 package com.violetbeach.studyrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudyRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyRestApiApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 
 }
